b50d13a7feef2a0bf18e903e6cd7bd063ade3d57
Jyevon <jyevon@outlook.de>
2024-07-25 19:03:03 +0200

    Refine code

--- REISUB/REISUB.ino
+++ REISUB/REISUB.ino
@@ -2,50 +2,62 @@
 
 #define LED_PIN 2
 
-long timeout = 10 * 60 * ((long) 1000); /*10 minutes*/
+long timeout = 10 * 60 * ((long) 1000); // 10 minutes
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  
   bool reisub = false;
+  
+  // === wait for serial connection or timeout ===
+  digitalWrite(LED_PIN, HIGH);
   
   Serial.begin(9600);
   while(!Serial) {
     if(millis() > timeout) {
       reisub = true;
-      delay(100);
       break;
     }
-  }
-  
-  Serial.print("Reboot prevented. ");
-  Serial.print((timeout - millis()) / 1000);
-  Serial.println(" seconds were left.");
-  
-  Serial.end();
 
-  if(!reisub) { // deactivate
-    while(true) {
-      digitalWrite(LED_PIN, HIGH);
-      delay(1000);
-      digitalWrite(LED_PIN, LOW);
-      delay(1000);
-    }
+    delay(100);
   }
 
   
+  // === serial connection established ===
+  if(!reisub) {
+    Serial.print("Reboot prevented. ");
+    Serial.print((timeout - millis()) / 1000);
+    Serial.println(" seconds were left.");
+    
+    Serial.end();
+
+    // become inactive
+    while(true)  blinkOnce();
+  }
+
+  
+  // === issue REISUB key sequence ===
   digitalWrite(LED_PIN, LOW);
 
+  // Give system time to detect serial disconnected
   delay(5000);
+
+  issueREISUB();
+
   
-  // Start the Keyboard library
+  // === REISUB issued ===
+  blinkOnce();
+  blinkOnce();
+}
+
+// Connect as keyboard, send REISUB key sequence, disconnect
+void issueREISUB() {
+  // Connect as keyboard
   Keyboard.begin();
 
-  // Give the system some time to recognize the keyboard
+  // Give system time to recognize the keyboard
   delay(5000);
 
   // Hold down Alt + Print Screen (SysRq)
@@ -53,35 +65,33 @@ void loop() {
   Keyboard.press(KEY_PRINT_SCREEN);
   delay(1000);
 
-  // Send the REISUB sequence with brief delays
-  sendKeyWithModifiers('r');
-  sendKeyWithModifiers('e');
-  sendKeyWithModifiers('i');
-  sendKeyWithModifiers('s');
-  sendKeyWithModifiers('u');
-  sendKeyWithModifiers('b');
+  // Send REISUB sequence with brief delays
+  typeKey('r');
+  typeKey('e');
+  typeKey('i');
+  typeKey('s');
+  typeKey('u');
+  typeKey('b');
 
   // Release all keys
   Keyboard.releaseAll();
 
-  // End the Keyboard library
+  // Disconnect as keyboard
   Keyboard.end();
-  
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
-  
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
 }
 
-void sendKeyWithModifiers(char key) {
-  // Send the key while holding Alt and Print Screen
+// Press and release key without releasing keys that are currently pressed
+void typeKey(char key) {
   Keyboard.press(key);
   delay(100);
   Keyboard.release(key);
-  delay(900);  // 1 second delay between each key press
+  delay(900);  // => 1 second delay between each key press
+}
+
+// Turn LED on and off once, with delay
+void blinkOnce() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
 }
